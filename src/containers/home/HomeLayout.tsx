ab0a576ba9a97e83026139a62c28e39bdd623b75
masterpranay1 <rajpranaychottu@gmail.com>
2024-10-20 22:25:19 +0530

    feat: start + requisition update

---
+++ src/containers/home/HomeLayout.tsx
@@ -0,0 +1,82 @@
+import {
+  Container,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Heading,
+  TabProps,
+  Box,
+  Grid,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import InterviewSettingsForm from "./InterviewSettingsForm";
+import JobDetailsForm from "./JobDetailsForm";
+import RequisitionForm from "./RequisitionDetailsForm";
+import DisplayCard from "./PreviewCard";
+import { PageNumbers } from "../../interface/home";
+import { IRequisitionDetails } from "@src/interface/forms";
+
+const CustomTab: React.FC<TabProps> = ({ children, ...props }) => {
+  return (
+    <Tab p="1rem" fontFamily="Poppins" {...props}>
+      {children}
+    </Tab>
+  );
+};
+
+const HomeLayout = () => {
+  const [page, setPage] = useState<PageNumbers>(0);
+
+  const handlePage = (pageNumber: PageNumbers) => {
+    setPage(pageNumber);
+  };
+
+  const [requisitionDetails, setRequisitionDetails] =
+    useState<IRequisitionDetails>({
+      requisitionTitle: "",
+      noOfOpenings: 0,
+      urgency: "",
+      gender: "",
+    });
+
+  return (
+    <Box w="100%">
+      <Container maxW="1200px">
+        <Heading fontFamily="Poppins" fontSize="1.5rem" my="2rem">
+          Create Candidate Requisition
+        </Heading>
+        <Tabs index={page} isLazy lazyBehavior="keepMounted">
+          <TabList>
+            <CustomTab>Requistion Details</CustomTab>
+            <CustomTab>Job Details</CustomTab>
+            <CustomTab>Interview Settings</CustomTab>
+          </TabList>
+          <Grid display="grid" gridTemplateColumns="3fr 2fr" gap="24px">
+            <TabPanels>
+              <TabPanel>
+                <RequisitionForm
+                  handleTab={handlePage}
+                  requisitionDetails={requisitionDetails}
+                  setRequisitionDetails={setRequisitionDetails}
+                />
+              </TabPanel>
+              <TabPanel>
+                <JobDetailsForm handleTab={handlePage} />
+              </TabPanel>
+              <TabPanel>
+                <InterviewSettingsForm handleTab={handlePage} />
+              </TabPanel>
+            </TabPanels>
+            <DisplayCard 
+              requisitionDetails={requisitionDetails} 
+            />
+          </Grid>
+        </Tabs>
+      </Container>
+    </Box>
+  );
+};
+
+export default HomeLayout;
